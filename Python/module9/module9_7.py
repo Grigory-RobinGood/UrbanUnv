b9dd12ddff3ec7c5b324ed4f5fb015182eb0aaff
python <kibersantus@mail.ru>
2024-09-29 19:52:33 +0300

    df1

---
+++ Python/module9/module9_7.py
@@ -0,0 +1,29 @@
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)
+        if result < 2:
+            print("Составное")
+            return result
+
+        for i in range(2, int(result ** 0.5) + 1):
+            if result % i == 0:
+                print("Составное")
+                return result
+
+        print("Простое")
+        return result
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+# Пример использования
+result = sum_three(2, 3, 6)
+print(result)  # Вывод: 11 (Простое)
+
+result = sum_three(4, 6, 8)
+print(result)  # Вывод: 18 (Составное)
